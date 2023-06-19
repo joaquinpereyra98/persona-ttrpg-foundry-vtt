bae8cd24ca827c5133713f9dfbe1566e8812f5e3
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2023-06-19 19:06:55 -0300

    replace and add basic files

---
+++ module/helpers/config.mjs
@@ -0,0 +1,31 @@
+export const BOILERPLATE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ BOILERPLATE.abilities = {
+  "ath": "BOILERPLATE.AbilityAth",
+  "pro": "BOILERPLATE.AbilityPro",
+  "gut": "BOILERPLATE.AbilityGut",
+  "kno": "BOILERPLATE.AbilityKno",
+  "cha": "BOILERPLATE.AbilityCha",
+  "str": "BOILERPLATE.AbilityStr",
+  "end": "BOILERPLATE.AbilityEnd",
+  "agi": "BOILERPLATE.AbilityAgi",
+  "mag": "BOILERPLATE.AbilityMag",
+  "luc": "BOILERPLATE.AbilityLuc"
+};
+
+BOILERPLATE.abilityAbbreviations = {
+  "ath": "BOILERPLATE.AbilityAthAbbr",
+  "pro": "BOILERPLATE.AbilityProAbbr",
+  "gut": "BOILERPLATE.AbilityGutAbbr",
+  "kno": "BOILERPLATE.AbilityKnoAbbr",
+  "cha": "BOILERPLATE.AbilityChaAbbr",
+  "str": "BOILERPLATE.AbilityStrAbbr",
+  "end": "BOILERPLATE.AbilityEndAbbr",
+  "agi": "BOILERPLATE.AbilityAgiAbbr",
+  "mag": "BOILERPLATE.AbilityMagAbbr",
+  "luc": "BOILERPLATE.AbilityLucAbbr"
+};
